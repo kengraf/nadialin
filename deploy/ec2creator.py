48bf40e79793660ede78c5035ce4006edce4ca5b
Ken Graf <kengraf57@gmail.com>
2025-01-31 16:14:35 -0500

    Update ec2creator.py

--- deploy/ec2creator.py
+++ deploy/ec2creator.py
@@ -23,7 +23,7 @@ else:
     
 response = ec2.describe_security_groups( Filters=[{'Name': 'tag:Name', 'Values': ['nadialinSecurityGroup']}])
 sgs = response.get('SecurityGroupIds', [])
-print(sgs)
+print(response)
 if sgs:
     instance_params["SecurityGroupIds"] = sgs[0]['SecurityGroupId']
 else:
