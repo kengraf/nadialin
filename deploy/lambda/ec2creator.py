301c172193b0e24eaa132c99cd0f31aae00b799d
Ken Graf <kengraf57@gmail.com>
2025-02-02 09:10:52 -0500

    Update ec2creator.py

--- deploy/lambda/ec2creator.py
+++ deploy/lambda/ec2creator.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     print(f"Fetched Item: {fetched_item}")
 
     item = {
-        "uuid": str(uuid.uuid4()), 
+        "uuid": f"{str(uuid.uuid4())}", 
         "type": "instance",
         "machine": uuid,
         "status": "running"
