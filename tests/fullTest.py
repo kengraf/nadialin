b01bdd061b1dd58194f5457b6bff9fa67d3d3e5d
Ken Graf <kengraf57@gmail.com>
2025-02-23 18:51:01 -0500

    Update fullTest.py

--- tests/fullTest.py
+++ tests/fullTest.py
@@ -203,11 +203,11 @@ def renew_instanceState():
 def event_scores():
     try:
         payload = {} # {"key1": "value1", "key2": "value2"}
+        print( f"{DEPLOY_NAME}-eventScores" )
         result = invoke_lambda(f"{DEPLOY_NAME}-eventScores", payload)
         print( f"{DEPLOY_NAME}-eventScores" )
         return True
     except Exception as e:
-        print("shit")
         return False
 
 
