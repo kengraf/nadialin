3ad8270e95d90d6211fb0878f7eb3cdc39535e04
Ken Graf <kengraf57@gmail.com>
2025-03-02 10:28:10 -0500

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -36,7 +36,8 @@ s3() {
         echo "Creating bucket '$S3BUCKET'."
         aws cloudformation deploy --stack-name ${DEPLOYNAME}-s3 --template-file s3.yaml \
             --capabilities CAPABILITY_NAMED_IAM --output text \
-            --parameter-overrides  S3BUCKET=$S3BUCKET DEPLOYNAME=$DEPLOYNAME DOMAINNAME=$DOMAINNAME
+            --parameter-overrides  S3BUCKET=$S3BUCKET DEPLOYNAME=$DEPLOYNAME \
+                DOMAINNAME=$DOMAINNAME CERTARN=$CERTARN
     fi
     
     echo "Uploading website content"
