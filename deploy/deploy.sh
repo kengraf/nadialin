66c51b2fa32d18f8a7ca32049b89a39a01ab695e
Ken Graf <kengraf57@gmail.com>
2025-04-04 20:59:02 -0400

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -59,7 +59,7 @@ cf() {
 
     aws cloudformation describe-stacks --stack-name ${DEPLOYNAME} | jq .Stacks[0].Outputs
 
-    echo "Update Gateway CORS settings
+    echo "Update Gateway CORS settings"
     API_ID = `aws apigatewayv2 get-apis --query "Items[?Name=='nadialin'].ApiId" --output text`
 
     aws apigatewayv2 update-api --api-id ${API_ID} \
