f8064cc88816290af438cddf2065eb602fd3b337
Ken Graf <kengraf57@gmail.com>
2025-08-14 14:56:44 -0400

    Update test_example.sh

--- deploy/templates/test_example.sh
+++ deploy/templates/test_example.sh
@@ -2,4 +2,4 @@
 # This script comfirms SSH ccess is still active
 # Removal of user or SSH files will cause a failure
 cd /home/example
-scp -i .ssh/scoring_key example@localhost:./.ssh/authorized_keys pub
+scp -o -i .ssh/scoring_key example@localhost:./.ssh/authorized_keys pub
