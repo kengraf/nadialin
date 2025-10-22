c099c78947901f2f2312b63fe37912b27a9a3855
Ken Graf <kengraf57@gmail.com>
2025-10-22 11:07:47 -0400

    replace cd with pushd/popd

--- deploy/templates/test_example.sh
+++ deploy/templates/test_example.sh
@@ -1,5 +1,6 @@
 #1/bin/bash
 # This script comfirms SSH ccess is still active
 # Removal of user or SSH files will cause a failure
-cd /home/example
+pushd /home/example
 scp -o -i .ssh/scoring_key example@localhost:./.ssh/authorized_keys pub
+popd
