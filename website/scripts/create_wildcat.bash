ca1631da7d53f2b68efad845c16dc41f4389a559
Ken Graf <kengraf57@gmail.com>
2025-10-26 18:39:16 -0400

    Update and rename create_bear.bash to create_wildcat.bash

---
+++ website/scripts/create_wildcat.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+# --- add bear user ---
+useradd --password $(openssl passwd passwordsAREwrong) wildcat
+su -c 'ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N "" -q' wildcat
+su -c 'mv ~/.ssh/id_rsa.pub  ~/.ssh/authorized_keys' wildcat
+
